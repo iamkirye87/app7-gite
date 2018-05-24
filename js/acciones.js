de9691970c111d5a69784f715bdd4b567a4302a6
araceliflores01 <38224775+araceliflores01@users.noreply.github.com>
2018-05-24 08:19:04 -0500

    subida4

--- js/acciones.js
+++ js/acciones.js
@@ -4,11 +4,11 @@ $(document).ready(function(e){
 	document.addEventListener("deviceready",function(){
 		
 		$('#izquierda').on("swipeleft",function(){
-			navigator.notification.alert("Deslizo a la izquierda",function(){"Aplicaion7","Aceptar"});
+			navigator.notification.alert("Deslizo a la izquierda",function(){"Aplicaion7","ok"});
 });
 
 $('#derecha').on("swiperight",function(){
-	navigator.notification.confirm("¿Que quieres hacer?",function(opt){
+	navigator.notification.confirm("¿Que deseas hacer?",function(opt){
 		switch(opt)
 		{
 			case 1:
